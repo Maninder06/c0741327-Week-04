04f8a65e728d46984526b351cf8942294080da49
visualstudio <visualstudio@230-PC19>
2019-01-31 11:59:52 -0500

    c0741327

--- c0741327/js/main.js
+++ c0741327/js/main.js
@@ -1,1 +1,8 @@
 ﻿// Your code here!
+var cat = new Date();
+
+function myfunction() {
+  
+    document.body.innerHTML = "<h1> the date is " + cat.getMonth() + "/" + cat.getDate() + "/" + cat.getFullYear() + "</h1>"
+}
+
